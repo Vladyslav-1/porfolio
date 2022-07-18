76644dac9410c329df607c18b3cef1fb3d87b2bb
Vladyslav <vladueno@gmail.com>
2022-07-18 13:19:35 +0300

    firs draft

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//     var scroll_pos = 210;
+//     $("#left-panel").scroll(function () {
+//         scroll_pos = $(this).scrollTop();
+//         if (scroll_pos > 210) {
+//             $("#sidepanel").css('fill', '#1A1A1A');
+//         } else {
+//             $("#sidepanel").css('fill', '#FFA501');
+//         }
+//         console.log(scroll_pos);
+//     });
+// });
