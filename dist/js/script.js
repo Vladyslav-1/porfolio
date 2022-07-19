b72c0ea392096ffb50b38ccf8dfc4db2f05d6b77
Vladyslav <vladueno@gmail.com>
2022-07-19 16:53:43 +0300

    finished_first_section

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,3 +10,17 @@
 //         console.log(scroll_pos);
 //     });
 // });
+
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
