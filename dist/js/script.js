63e300dead7ead2a235020f069507b237b287b98
Vladyslav <vladueno@gmail.com>
2022-07-22 12:12:32 +0300

    addjavascript

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,32 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-      if (document.body.scrollTop >= 500) {
-        $("sidepanel__link svg").css({
-          'fill':'#000000'
-        });
-      };
-    });
-  });
-  
+// Скрипт, чтобы эллемент менял цвет после определенного количества прокрутки
+const linkStyle = document.querySelectorAll('.sidepanel__link svg path')
+//Коллекция дом элементов, т.е все элементы, у которых есть такой класс 
+//Вешаем обработчик событий на все окно, как только достигнем 500 пикселей, добавим всем элементам инлайн стилей
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset >=500){
+    linkStyle.forEach(el => el.style.fill = 'black')
+  }else{
+    linkStyle.forEach(el => el.style.fill='')
+  }
+})
+const TextMenu = document.querySelectorAll('.sidepanel__text span')
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset >=300){
+    TextMenu.forEach(el => el.style.color = 'black')
+  }else{
+    TextMenu.forEach(el => el.style.color='')
+  }
+})
+const MenuDevider = document.querySelectorAll('.sidepanel__divider')
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset >=400){
+    MenuDevider.forEach(el => el.style.backgroundColor = 'black')
+  }else{
+    MenuDevider.forEach(el => el.style.backgroundColor='')
+  }
+})
 
-
+// Hamburger
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
